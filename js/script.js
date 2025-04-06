4904be86c99c1b9a68eed38d5a45f28f21048339
first <garvitjainraju@gmail.com>
2025-04-07 03:06:54 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-
-
 function updateImageBasedOnTheme() {
-	const theme = localStorage.getItem('theme'); // Retrieve theme from local storage
-	const image = document.getElementById('logo-img');
+  localStorage.setItem("theme", "light"); // Set theme to light for testing
+  const theme = localStorage.getItem("theme"); // Retrieve theme from local storage
+  const image = document.getElementById("logo-img");
 
-	if (theme === 'light') {
-		// If the theme is light, make the image black
-		image.classList.add('darken-image');
-	} else {
-		// Do nothing for dark theme
-		image.classList.remove('darken-image');
-	}
+  if (theme === "light") {
+    // If the theme is light, make the image black
+    image.classList.add("darken-image");
+  } else {
+    // Do nothing for dark theme
+    image.classList.remove("darken-image");
+  }
 }
 
 // Run the function on page load
